688615e62786e862511216b454a0fc047d9dd8b0
alpharr <tcmpvh8@gmail.com>
2020-06-03 17:25:23 -0400

    aa

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ bot.on("message", message=>{
 
     switch(args[0]){
         case "apply":
-            message.channel.send("Sent you a msg in dms. If you have dms off, you'll need to enable them.");
+            message.channel.send("We have sent you a message in DMs. If you have DMs off, you will need to enable them. Thank you!");
             message.author.send("Staff applications guidelines URL: https://docs.google.com/document/d/16n0oGAoVG5p_By4Sfmyjf97HgW6rRwuzIsMY4BpVw-E/edit?usp=sharing \n - YouTuber Apps URL: https://docs.google.com/forms/d/e/1FAIpQLSfwCxUJIPMeKJKtDCO8bnOgIWy_wB_QbHecuYE7jbXQ16pAnQ/viewform \n - Staff apps URL: https://docs.google.com/forms/d/e/1FAIpQLSfv0aTscDr7WCIO8BpZhNqsS7XnBjtU9LgRE9S5yG52IORl_A/viewform?usp=sf_link \n - Ban appeals: https://forms.gle/kShUxptQ4UZTLd4m7 \n - Builder Apps URL: https://docs.google.com/forms/d/e/1FAIpQLSeAD7Fo09n-fw4gCJneMMfrzZDlqzK83cZbCb256JxsGyMI8g/viewform");
         break;
     }
